22d231f203b9340fcf5cbdd11933c1ab0bceea30
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-04-18 23:38:22 +0900

    add styles to the detail page

--- src/routes/CoinDetail.tsx
+++ src/routes/CoinDetail.tsx
@@ -69,24 +69,74 @@ const Box = styled.div`
   border-radius: 5px;
 `;
 const Overview = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
 `;
 
 const OverviewLeft = styled(Box)`
-  width: 60%;
   display: flex;
+  justify-content: space-between;
+  padding: 20px;
 `;
 const Detail = styled.div`
-  width: 60%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  :nth-child(1) {
+    display: flex;
+    align-items: center;
+    img {
+      margin-right: 4px;
+    }
+  }
+  :nth-child(2) {
+    h2 {
+      font-weight: 700;
+      font-size: 30px;
+      margin-right: 5px;
+    }
+  }
 `;
+
+const Rank = styled.span`
+  border: 1px solid #4cd137;
+  border-radius: 5px;
+  padding: 3px;
+  margin-left: 5px;
+  color: #4cd137;
+  font-weight: 500;
+  font-size: 13px;
+`;
+
 const DailyPrice = styled.div`
-  width: 40%;
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
 `;
+
+const DailyPriceDetail = styled.div`
+  display: flex;
+  justify-content: space-between;
+  :first-child {
+    margin-right: 25px;
+  }
+  &:last-child {
+    color: #4834d4;
+  }
+`;
+
 const OverviewRight = styled(Box)`
-  width: 40%;
+  display: flex;
 `;
-const CapInfo = styled.div``;
-const SupplyInfo = styled.div``;
+const CapInfo = styled.div`
+  span {
+    display: block;
+  }
+`;
+const SupplyInfo = styled.div`
+  span {
+    display: block;
+  }
+`;
 
 const Tabs = styled.div`
   margin: 20px 0px;
@@ -100,14 +150,14 @@ const Tab = styled.span<{ isActive: boolean }>`
   background-color: #7f8fa6;
   padding: 8px;
   border-radius: 15px;
-  color: ${(props) => (props.$isActive ? props.theme.accentColor : props.theme.textColor)}
+  color: ${(props) => (props.isActive ? props.theme.accentColor : props.theme.textColor)}
     a {
     display: block;
   }
 `;
 
-const Description = styled.div`
-  margin: 20px 0px;
+const Description = styled(Box)`
+  padding: 20px;
   h1 {
     margin-bottom: 10px;
   }
@@ -120,21 +170,42 @@ const Description = styled.div`
 
 function CoinDetail() {
   const { coinId } = useParams() as IRouteParams;
-  const { state } = useLocation() as ILocationParams;
   const [loading, setLoading] = useState(true);
   const [coinInfo, setCoinInfo] = useState<ICoinInfo>({});
   const priceMatch = useMatch('/:coinId/price');
   const chartMatch = useMatch('/:coinId/chart');
 
+  // useEffect(() => {
+  //   const fetchCoinInfo = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `https://api.coingecko.com/api/v3/coins/${coinId}?localization=false`
+  //       );
+  //       if (!response.ok) {
+  //         throw new Error('Failed to fetch coin data');
+  //       }
+  //       const coinData = await response.json();
+  //       setCoinInfo(coinData);
+  //     } catch (error) {
+  //       console.error('Error while fetching coin data: ', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchCoinInfo();
+  // }, [coinId]);
+
   useEffect(() => {
     (async () => {
-      const coinData = await (
+      const response = await (
         await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}?localization=false`)
       ).json();
-      setCoinInfo(coinData);
+      setCoinInfo(response);
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
+
   return (
     <Container>
       {loading ? (
@@ -148,54 +219,62 @@ function CoinDetail() {
                   <img src={coinInfo.image.thumb} alt={coinInfo.name} />
                   <span>
                     {coinInfo.name} / {coinInfo.symbol.toUpperCase()}
-                    <span>Rank #{coinInfo.market_data.market_cap_rank}</span>
                   </span>
+                  <Rank>Rank #{coinInfo.market_data.market_cap_rank}</Rank>
                 </div>
                 <div>
-                  <span>${coinInfo.market_data.current_price.usd}</span>
-                  <span>
-                    {coinInfo.market_data.price_change_percentage_24h.toFixed(2)}%
-                  </span>
+                  <h2>${coinInfo.market_data?.current_price?.usd?.toLocaleString()}</h2>
                 </div>
               </Detail>
               <DailyPrice>
-                <div>
-                  <span>High</span>
-                  <span>${coinInfo.market_data.high_24h.usd}</span>
-                </div>
-                <div>
-                  <span>Low</span>
-                  <span>${coinInfo.market_data.low_24h.usd}</span>
-                </div>
+                <DailyPriceDetail>
+                  <span>24h %</span>
+                  <span>
+                    {coinInfo.market_data?.price_change_percentage_24h.toFixed(2)}%
+                  </span>
+                </DailyPriceDetail>
+                <DailyPriceDetail>
+                  <span>24h High</span>
+
+                  <span>${coinInfo.market_data?.high_24h.usd?.toLocaleString()}</span>
+                </DailyPriceDetail>
+                <DailyPriceDetail>
+                  <span>24h Low</span>
+                  <span>${coinInfo.market_data?.low_24h.usd?.toLocaleString()}</span>
+                </DailyPriceDetail>
               </DailyPrice>
             </OverviewLeft>
             <OverviewRight>
               <CapInfo>
                 <div>
                   <span>Market Cap</span>
-                  <span>${coinInfo.market_data.market_cap.usd}</span>
+                  <span>${coinInfo.market_data?.market_cap.usd?.toLocaleString()}</span>
                 </div>
                 <div>
                   <span>Fully Diluted Valuation</span>
-                  <span>${coinInfo.market_data.fully_diluted_valuation.usd}</span>
+                  <span>
+                    ${coinInfo.market_data?.fully_diluted_valuation.usd?.toLocaleString()}
+                  </span>
                 </div>
                 <div>
                   <span>Total Volume</span>
-                  <span>${coinInfo.market_data.total_volume.usd}</span>
+                  <span>${coinInfo.market_data?.total_volume.usd?.toLocaleString()}</span>
                 </div>
               </CapInfo>
               <SupplyInfo>
                 <div>
                   <span>Total Supply</span>
-                  <span>${coinInfo.market_data.total_supply}</span>
+                  <span>${coinInfo.market_data?.total_supply?.toLocaleString()}</span>
                 </div>
                 <div>
                   <span>Max Supply</span>
-                  <span>${coinInfo.market_data.max_supply}</span>
+                  <span>${coinInfo.market_data?.max_supply?.toLocaleString()}</span>
                 </div>
                 <div>
                   <span>Circulating Supply</span>
-                  <span>${coinInfo.market_data.circulating_supply}</span>
+                  <span>
+                    ${coinInfo.market_data?.circulating_supply?.toLocaleString()}
+                  </span>
                 </div>
               </SupplyInfo>
             </OverviewRight>
@@ -220,4 +299,5 @@ function CoinDetail() {
     </Container>
   );
 }
+
 export default CoinDetail;
